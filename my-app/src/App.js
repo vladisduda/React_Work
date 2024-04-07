e00fc0a4739953213aacdd44f53c958dbbb3e164
vladisduda <vladisduda@mail.ru>
2024-04-07 22:19:39 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 function App() {
-	// код компонента
+	return (
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+			<li>5</li>
+			<li>6</li>
+			<li>7</li>
+			<li>8</li>
+			<li>9</li>
+			<li>10</li>
+		</ul>
+	)
 }
 
 export default App
