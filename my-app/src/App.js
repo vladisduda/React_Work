fa4d60293b13f5e6328700061492c6990648aebc
vladisduda <vladisduda@mail.ru>
2024-04-07 22:16:07 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 function App() {
-	// код компонента
+	const elem = (
+		<div>
+			<p class='крутой'>крутой тэкст 1</p>
+			<p class='еще круче'>если тэкст 1 такой крутой, почему нет тэкст 2?</p>
+			<p class='позер'>
+				если тэкст 2 такой же крутой, как тэкст 1, то почему нет тэкст 3?
+			</p>
+		</div>
+	)
+
+	return elem
 }
 
 export default App
