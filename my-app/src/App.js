d323617a0149e4ccc96f0da477b43e6c0708a003
vladisduda <vladisduda@mail.ru>
2024-04-08 19:47:04 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react'
 
+// Задание 1 - как работает код:
+// В компоненте App определена функция func принимающая два параметра: arg и event.
+// В JSX элементе кнопки используется обработчик события onClick, который вызывает стрелочную функцию.
+// Внутри функции вызывается func('eee', event), передавая строку 'eee' первым аргументом и event вторым.
+// При нажатии на кнопку будет выведена строка 'eee' в консоль.
+
 function App() {
-	function func(event) {
-		console.log(event) // объект с событием
+	function func1(event, arg) {
+		console.log(arg, event)
 	}
-	function func1(event) {
-		console.log(event.target) // event target of the click
+
+	function func2(arg1, event, arg2) {
+		console.log(arg1, event, arg2)
 	}
+
+	function func3(arg1, arg2) {
+		console.log(arg1, arg2)
+	}
+
 	return (
 		<div>
-			<button onClick={func}>event</button>
-			<button onClick={func1}>event.target</button>
+			<button onClick={event => func1(event, 'eee')}>1</button>
+			<button onClick={event => func2('hello', event, 'eee')}>2</button>
+			<button onClick={() => func3('eee', 'hello')}>3</button>
 		</div>
 	)
 }
