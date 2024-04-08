d10113195f2898d362d798d1598cf44260889af2
vladisduda <vladisduda@mail.ru>
2024-04-08 19:56:59 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,9 @@ import React from 'react'
 function App() {
 	const arr = ['a', 'b', 'c', 'd', 'e']
 
-	return (
-		<ul>
-			{arr.map((item, index) => (
-				<li key={index}>{item}</li>
-			))}
-		</ul>
-	)
+	const listItems = arr.map((item, index) => <li key={index}>{item}</li>)
+
+	return <ul>{listItems}</ul>
 }
+
 export default App
