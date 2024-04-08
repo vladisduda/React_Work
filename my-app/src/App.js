eaa8b5a4234f0ac6cb8d85096b633b0001cb9c6f
vladisduda <vladisduda@mail.ru>
2024-04-08 16:57:33 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,17 @@
 import React from 'react'
 
-function App() {
-	const one = (
-		<div>
-			<ul>
-				<li>1</li>
-				<li>2</li>
-				<li>3</li>
-				<li>4</li>
-				<li>5</li>
-				<li>6</li>
-				<li>7</li>
-				<li>8</li>
-				<li>9</li>
-				<li>10</li>
-			</ul>
-		</div>
-	)
+// Можно сделать так, чтобы в переменной хранился не текст, а тег:
 
-	const two = (
-		<div>
-			<table>
-				<tbody>
-					<tr>
-						<td>11</td>
-						<td>12</td>
-						<td>13</td>
-					</tr>
-					<tr>
-						<td>21</td>
-						<td>22</td>
-						<td>23</td>
-					</tr>
-					<tr>
-						<td>31</td>
-						<td>32</td>
-						<td>33</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	)
-	return (
-		<div>
-			{one},{two}
-		</div>
-	)
+function App() {
+	let text
+	const show = true
+
+	if (show) {
+		text = <p>text1</p>
+	} else {
+		text = <p>text2</p>
+	}
+
+	return <div>{text}</div>
 }
 export default App
