5ecc98e1acabd2ee4eff419f673cec5f6cfeed1b
vladisduda <vladisduda@mail.ru>
2024-04-08 19:20:03 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
-function App() {
-	const isAuth = false
 
-	return <div>{isAuth === false && <p>Пожалуйста, авторизуйтесь</p>}</div>
+function App() {
+	function getDigitsSum(a, b, c) {
+		return a + b + c
+	}
+	const sum = getDigitsSum(1, 2, 3)
+	return <div>сумму цифр числа 123 = {sum}</div>
 }
 
 export default App
