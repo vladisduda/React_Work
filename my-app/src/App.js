ea2b8cbb163c9c540bd8f5b41be4857cb8f818f0
vladisduda <vladisduda@mail.ru>
2024-04-08 15:48:11 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
 
 function App() {
-	const arr = [1, 2, 3, 4, 5]
+	const obj = { name: 'john', surname: 'smit' }
 
 	return (
-		<ul>
-			<li>{arr[0]}</li>
-			<li>{arr[1]}</li>
-			<li>{arr[2]}</li>
-			<li>{arr[3]}</li>
-			<li>{arr[4]}</li>
-		</ul>
+		<p>
+			name: <span>{obj.name}</span>, <br />
+			surname: <span>{obj.surname}</span>
+		</p>
 	)
 }
 
