5f9bb7b87f0d887a9659f7a711fb2877b716aa7b
vladisduda <vladisduda@mail.ru>
2024-04-08 17:05:32 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
 function App() {
-	const isAdmin = true
+	const age = 19
 
-	return <div>{isAdmin && <p>Вы администратор!</p>}</div>
+	return (
+		<div>
+			{age > 18 ? (
+				<p>Вы совершеннолетний!</p>
+			) : (
+				<p>Вы еще не достигли 18 лет.</p>
+			)}
+		</div>
+	)
 }
 
 export default App
