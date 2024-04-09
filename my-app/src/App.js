2729b98f2b27850983b4bede8ccc05ffbb93a734
vladisduda <vladisduda@mail.ru>
2024-04-09 16:22:08 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 function App() {
 	const users = [
-		{ name: 'user1', surn: 'surn1', age: 30 },
-		{ name: 'user2', surn: 'surn2', age: 31 },
-		{ name: 'user3', surn: 'surn3', age: 32 },
+		{ id: 1, name: 'user1', surn: 'surn1', age: 30 },
+		{ id: 2, name: 'user2', surn: 'surn2', age: 31 },
+		{ id: 3, name: 'user3', surn: 'surn3', age: 32 },
 	]
 
 	return (
 		<ul>
-			{users.map((user, index) => (
-				<li key={index}>
-					name: {user.name}, surn: {user.surn}, age: {user.age}
+			{users.map(user => (
+				<li key={user.id}>
+					Имя: {user.name}, Фамилия: {user.surn}, Возраст: {user.age}
 				</li>
 			))}
 		</ul>
