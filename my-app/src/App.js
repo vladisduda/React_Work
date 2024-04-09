49a4045eef7049b6a949a68a4dfa39b9d20a33c5
vladisduda <vladisduda@mail.ru>
2024-04-09 16:34:44 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function App() {
-	const users = [
-		{ id: 1, name: 'user1', surn: 'surn1', age: 30 },
-		{ id: 2, name: 'user2', surn: 'surn2', age: 31 },
-		{ id: 3, name: 'user3', surn: 'surn3', age: 32 },
-	]
+const App = () => {
+	const [name, setName] = useState('Максим')
+	const [surname, setSurname] = useState('Русый')
+	const [age, setAge] = useState('14')
 
 	return (
-		<table>
-			<thead>
-				<tr>
-					<th>id</th>
-					<th>name</th>
-					<th>surn</th>
-					<th>age</th>
-				</tr>
-			</thead>
-			<tbody>
-				{users.map(user => (
-					<tr key={user.id}>
-						<td>{user.id}</td>
-						<td>{user.name}</td>
-						<td>{user.surn}</td>
-						<td>{user.age}</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
+		<div>
+			<p>Имя: {name}</p>
+			<p>Фамилия: {surname}</p>
+			<p>Возраст: {age}</p>
+		</div>
 	)
 }
 
