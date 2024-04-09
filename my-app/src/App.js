739ef6d024215e205797647f15f524f79f082332
vladisduda <vladisduda@mail.ru>
2024-04-09 16:48:32 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 
 const App = () => {
-	const [name, setName] = useState('Максим')
-	const [surname, setSurname] = useState('Русый')
-	const [age, setAge] = useState('14')
+	const [name, setName] = useState('Имя')
+	const [surname, setSurname] = useState('Фамилия')
+
+	const changeName = () => setName('Новое имя')
+	const changeSurname = () => setSurname('Новая фамилия')
 
 	return (
 		<div>
 			<p>Имя: {name}</p>
 			<p>Фамилия: {surname}</p>
-			<p>Возраст: {age}</p>
+			<button onClick={changeName}>Изменить имя</button>
+			<button onClick={changeSurname}>Изменить фамилию</button>
 		</div>
 	)
 }
