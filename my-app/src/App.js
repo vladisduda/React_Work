c76d34e0cfa8d3fc13aaf772c04d268519c9a808
vladisduda <vladisduda@mail.ru>
2024-04-09 17:00:00 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,16 @@
 import React, { useState } from 'react'
 
 function App() {
-	const [input1, setInput1] = useState('')
-	const [input2, setInput2] = useState('')
+	const [text, setText] = useState('')
 
-	function handleInput1Change(event) {
-		setInput1(event.target.value)
-	}
-
-	function handleInput2Change(event) {
-		setInput2(event.target.value)
+	function handleChange(event) {
+		setText(event.target.value)
 	}
 
 	return (
 		<div>
-			<input value={input1} onChange={handleInput1Change} />
-			<p>text1: {input1}</p>
-			<input value={input2} onChange={handleInput2Change} />
-			<p>text2: {input2}</p>
+			<input type='text' value={text} onChange={handleChange} />
+			<p>Количество символов: {text.length}</p>
 		</div>
 	)
 }
