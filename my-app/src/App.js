80b6942a985add921d04bf932caae12a1c4dd0a8
vladisduda <vladisduda@mail.ru>
2024-04-10 19:20:27 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,35 @@
 import React, { useState } from 'react'
 
 function App() {
-	const [value, setValue] = useState('чикибамбони')
-	const [checked, setChecked] = useState(true)
+	const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+	function changeHandler(index, event) {
+		setNotes([
+			...notes.slice(0, index),
+			event.target.value,
+			...notes.slice(index + 1),
+		])
+	}
+	function getARM(arr) {
+		const sum = arr.reduce((acc, val) => acc + parseFloat(val), 0)
+		const average = sum / arr.length
+		return average
+	}
+	const result = notes.map((note, index) => {
+		return (
+			<input
+				key={index}
+				value={note}
+				onChange={event => changeHandler(index, event)}
+			/>
+		)
+	})
+
 	return (
 		<div>
-			<p>текст по умолчанию:</p>
-			<input defaultValue={value} />
-			<p>по умолчанию "галочка" нажата:</p>
-			<input type='checkbox' defaultChecked={checked} />
+			<p>Cреднее арифметическое элементов = {getARM(notes)}</p>
+			<br />
+			{result}
 		</div>
 	)
 }
