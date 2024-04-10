46f77e46311175f423e8bac292816a26018fbb06
vladisduda <vladisduda@mail.ru>
2024-04-10 19:23:44 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,42 @@
 import React, { useState } from 'react'
 
 function App() {
-	const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-	function changeHandler(index, event) {
-		setNotes([
-			...notes.slice(0, index),
-			event.target.value,
-			...notes.slice(index + 1),
-		])
-	}
-	function getARM(arr) {
-		const sum = arr.reduce((acc, val) => acc + parseFloat(val), 0)
-		const average = sum / arr.length
-		return average
-	}
-	const result = notes.map((note, index) => {
-		return (
-			<input
-				key={index}
-				value={note}
-				onChange={event => changeHandler(index, event)}
-			/>
-		)
+	const [date, setDate] = useState({
+		year: 2025,
+		month: 12,
+		day: 31,
 	})
+
+	function handleInputChange(event, field) {
+		const newValue = parseInt(event.target.value, 10)
+		setDate({ ...date, [field]: newValue })
+	}
+
+	const dateObject = new Date(date.year, date.month - 1, date.day)
+	const dayOfWeek = dateObject.toLocaleString('default', { weekday: 'long' })
 
 	return (
 		<div>
-			<p>Cреднее арифметическое элементов = {getARM(notes)}</p>
-			<br />
-			{result}
+			<p>Год: {date.year}</p>
+			<p>Месяц: {date.month}</p>
+			<p>День: {date.day}</p>
+			<p>День недели: {dayOfWeek}</p>
+
+			<input
+				type='number'
+				value={date.year}
+				onChange={e => handleInputChange(e, 'year')}
+			/>
+			<input
+				type='number'
+				value={date.month}
+				onChange={e => handleInputChange(e, 'month')}
+			/>
+			<input
+				type='number'
+				value={date.day}
+				onChange={e => handleInputChange(e, 'day')}
+			/>
 		</div>
 	)
 }
