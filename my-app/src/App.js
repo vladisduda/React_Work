79d51e0cc1d79fa70874cbc9b87cee58f9744884
vladisduda <vladisduda@mail.ru>
2024-04-10 18:49:47 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,17 @@
 import React, { useState } from 'react'
 
 function App() {
+	const texts = ['Tokyo', 'New York', 'London', 'Paris']
 	const [value, setValue] = useState('')
 
-	function handleChange(event) {
-		setValue(event.target.value)
-	}
+	const options = texts.map((text, index) => {
+		return <option key={index}>{text}</option>
+	})
 
 	return (
 		<div>
-			<select value={value} onChange={handleChange}>
-				<option>Tokyo</option>
-				<option>New York</option>
-				<option>London</option>
-				<option>Paris</option>
+			<select value={value} onChange={event => setValue(event.target.value)}>
+				{options}
 			</select>
 			<p>выбор пользователя: {value}</p>
 		</div>
