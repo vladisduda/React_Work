466bdcecd0e557b7c21bf0c6be86dcfd143f311a
vladisduda <vladisduda@mail.ru>
2024-04-10 19:46:23 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,50 +1,65 @@
 import React, { useState } from 'react'
 
 function App() {
-	//№1
 	const [notes, setNotes] = useState([1, 2, 3, 4, 5])
+	const [newNoteText, setNewNoteText] = useState('')
 
 	function addNewNote() {
-		const newNote = notes.length + 1
-		setNotes([...notes, newNote])
+		if (newNoteText.trim()) {
+			setNotes([...notes, newNoteText])
+			setNewNoteText('')
+		}
 	}
 
-	const result = notes.map((note, index) => {
-		return <li key={index}>{note}</li>
-	})
+	function handleInputChange(event, index) {
+		const updatedNotes = [...notes]
+		updatedNotes[index] = event.target.value
+		setNotes(updatedNotes)
+	}
+
+	function handleInputBlur(event, index) {
+		const updatedNotes = [...notes]
+		updatedNotes[index] = event.target.value
+		setNotes(updatedNotes)
+	}
+
+	function reverseOrder() {
+		setNotes([...notes].reverse())
+	}
+
+	const noteItems = notes.map((note, index) => (
+		<li key={index}>
+			<input
+				type='text'
+				value={note}
+				onChange={e => handleInputChange(e, index)}
+				onBlur={e => handleInputBlur(e, index)}
+			/>
+			<button onClick={() => setNotes(notes.filter((_, i) => i !== index))}>
+				Удалить
+			</button>
+			<button
+				onClick={() =>
+					setNotes(notes.map((n, i) => (i === index ? n ** 2 : n)))
+				}
+			>
+				Возвести в квадрат
+			</button>
+		</li>
+	))
 
 	return (
 		<div>
-			<ul>{result}</ul>
+			<ul>{noteItems}</ul>
+			<input
+				type='text'
+				value={newNoteText}
+				onChange={e => setNewNoteText(e.target.value)}
+			/>
 			<button onClick={addNewNote}>Добавить элемент</button>
+			<button onClick={reverseOrder}>Перевернуть порядок</button>
 		</div>
 	)
-	//№2
-	// const [notes, setNotes] = useState([1, 2, 3, 4, 5])
-	// 	const [newNoteText, setNewNoteText] = useState('')
-
-	// 	function addNewNote() {
-	// 		if (newNoteText.trim()) {
-	// 			setNotes([...notes, newNoteText])
-	// 			setNewNoteText('')
-	// 		}
-	// 	}
-
-	// 	const result = notes.map((note, index) => {
-	// 		return <li key={index}>{note}</li>
-	// 	})
-
-	// 	return (
-	// 		<div>
-	// 			<ul>{result}</ul>
-	// 			<input
-	// 				type='text'
-	// 				value={newNoteText}
-	// 				onChange={e => setNewNoteText(e.target.value)}
-	// 			/>
-	// 			<button onClick={addNewNote}>Добавить элемент</button>
-	// 		</div>
-	// 	)
 }
 
 export default App
