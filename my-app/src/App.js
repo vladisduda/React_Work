5cc1d8c48c36f9447273999894a67c0badaab0b7
vladisduda <vladisduda@mail.ru>
2024-04-10 20:10:57 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,49 +2,33 @@ import React, { useState } from 'react'
 
 const initNotes = [
 	{
-		id: 'GYi9G_uC4gBF1e2SixDvu',
-		prop1: 'value11',
-		prop2: 'value12',
-		prop3: 'value13',
+		id: 1,
+		name: 'name1',
+		desc: 'long description 1',
+		show: false,
 	},
 	{
-		id: 'IWSpfBPSV3SXgRF87uO74',
-		prop1: 'value21',
-		prop2: 'value22',
-		prop3: 'value23',
+		id: 2,
+		name: 'name2',
+		desc: 'long description 2',
+		show: false,
 	},
 	{
-		id: 'JAmjRlfQT8rLTm5tG2m1L',
-		prop1: 'value31',
-		prop2: 'value32',
-		prop3: 'value33',
+		id: 3,
+		name: 'name3',
+		desc: 'long description 3',
+		show: false,
 	},
 ]
 
 function App() {
 	const [notes, setNotes] = useState(initNotes)
-	const [prop1, setProp1] = useState('')
-	const [prop2, setProp2] = useState('')
-	const [prop3, setProp3] = useState('')
 
-	const deleteNote = id => {
-		setNotes(notes.filter(note => note.id !== id))
-	}
-
-	const loadNoteToInputs = id => {
-		const note = notes.find(note => note.id === id)
-		setProp1(note.prop1)
-		setProp2(note.prop2)
-		setProp3(note.prop3)
-	}
-
-	const updateNoteFromInputs = id => {
+	const toggleShow = id => {
 		setNotes(
 			notes.map(note => {
 				if (note.id === id) {
-					note.prop1 = prop1
-					note.prop2 = prop2
-					note.prop3 = prop3
+					note.show = !note.show
 				}
 				return note
 			})
@@ -53,37 +37,15 @@ function App() {
 
 	return (
 		<div>
-			<ul>
-				{notes.map(note => (
-					<li key={note.id}>
-						<span>{note.prop1}</span>
-						<span>{note.prop2}</span>
-						<span>{note.prop3}</span>
-						<button onClick={() => deleteNote(note.id)}>Удалить</button>
-						<button onClick={() => loadNoteToInputs(note.id)}>
-							Загрузить в инпуты
-						</button>
-						<button onClick={() => updateNoteFromInputs(note.id)}>
-							Обновить из инпутов
-						</button>
-					</li>
-				))}
-			</ul>
-			<input
-				type='text'
-				value={prop1}
-				onChange={e => setProp1(e.target.value)}
-			/>
-			<input
-				type='text'
-				value={prop2}
-				onChange={e => setProp2(e.target.value)}
-			/>
-			<input
-				type='text'
-				value={prop3}
-				onChange={e => setProp3(e.target.value)}
-			/>
+			{notes.map(note => (
+				<p key={note.id}>
+					{note.name},{note.show && <i>{note.desc}</i>}
+					<br />
+					<button onClick={() => toggleShow(note.id)}>
+						{note.show ? 'Скрыть описание' : 'Показать описание'}
+					</button>
+				</p>
+			))}
 		</div>
 	)
 }
