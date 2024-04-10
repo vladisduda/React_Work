dad12ba73b461cac1cf4ee006713fec077535de0
vladisduda <vladisduda@mail.ru>
2024-04-10 18:54:12 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 
 function App() {
-	const texts = ['Tokyo', 'New York', 'London', 'Paris']
 	const [value, setValue] = useState('')
-
-	const options = texts.map((text, index) => {
-		return <option key={index}>{text}</option>
-	})
 
 	return (
 		<div>
 			<select value={value} onChange={event => setValue(event.target.value)}>
-				{options}
+				<option value='1'>от 0 до 12 лет</option>
+				<option value='2'>от 13 до 17</option>
+				<option value='3'>от 18 до 25</option>
+				<option value='4'>старше 25 лет</option>
 			</select>
-			<p>выбор пользователя: {value}</p>
+			<p>
+				{value === '1' && 'вы выбрали первый пункт'}
+				{value === '2' && 'вы выбрали второй пункт'}
+				{value === '3' && 'вы выбрали третий пункт'}
+				{value === '4' && 'вы скуф'}
+			</p>
 		</div>
 	)
 }
