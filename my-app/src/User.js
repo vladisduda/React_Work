5f1c40a0a96ad302afcef8f3055669d92989c613
vladisduda <vladisduda@mail.ru>
2024-04-16 18:55:13 +0300

    -

--- my-app/src/User.js
+++ my-app/src/User.js
@@ -3,6 +3,7 @@ import React from 'react'
 const User = ({ user }) => {
 	return (
 		<div className='user'>
+			<p>id: {user.id}</p>
 			<p>Name: {user.name}</p>
 			<p>Surname: {user.surname}</p>
 			<p>Age: {user.age}</p>
