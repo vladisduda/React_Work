97715b40f5f60848dd7330712679cc079c3b16a3
vladisduda <vladisduda@mail.ru>
2024-04-16 18:21:53 +0300

    -

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,12 @@
-import React, { useState } from 'react'
-
-const initNotes = [
-	{
-		id: 1,
-		name: 'name1',
-		desc: 'long description 1',
-		show: false,
-	},
-	{
-		id: 2,
-		name: 'name2',
-		desc: 'long description 2',
-		show: false,
-	},
-	{
-		id: 3,
-		name: 'name3',
-		desc: 'long description 3',
-		show: false,
-	},
-]
+import React from 'react'
+import Product from './Product'
+import User from './User' // Подключаем компонент User
 
 function App() {
-	const [notes, setNotes] = useState(initNotes)
-
-	const toggleShow = id => {
-		setNotes(
-			notes.map(note => {
-				if (note.id === id) {
-					note.show = !note.show
-				}
-				return note
-			})
-		)
-	}
-
 	return (
 		<div>
-			{notes.map(note => (
-				<p key={note.id}>
-					{note.name},{note.show && <i>{note.desc}</i>}
-					<br />
-					<button onClick={() => toggleShow(note.id)}>
-						{note.show ? 'Скрыть описание' : 'Показать описание'}
-					</button>
-				</p>
-			))}
+			<Product />
+			<User /> {/* Выводим компонент User */}
 		</div>
 	)
 }
---
+++ my-app/src/User.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function User() {
+	return (
+		<div>
+			<p>какой-нибудь текст из User</p>
+		</div>
+	)
+}
+
+export default User
