7c9c0216ff67c88d2339ddff38223fb47c9b103f
vladisduda <vladisduda@mail.ru>
2024-04-17 20:03:57 +0300

    -

--- my-app/src/User.js
+++ my-app/src/User.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const User = ({ user }) => {
+	const [isBanned, setIsBanned] = useState(false)
+
+	const handleBanClick = () => {
+		setIsBanned(true)
+	}
+
 	return (
 		<div className='user'>
 			<p>id: {user.id}</p>
 			<p>Name: {user.name}</p>
 			<p>Surname: {user.surname}</p>
 			<p>Age: {user.age}</p>
+			{isBanned ? (
+				<p style={{ color: 'red' }}>Пользователь в бане</p>
+			) : (
+				<button onClick={handleBanClick}>Забанить пользователя</button>
+			)}
 			<br />
 		</div>
 	)
